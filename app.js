0e66d2117ed0a79e37ad331c718365e7f9b994a4
Larssies <larsvanwylick@gmail.com>
2025-01-31 23:51:19 +0100

    Initial commit of my Node.js + EJS template

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const app = express();
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+});
+app.use(limiter);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+
+const indexRoute = require('./routes/index');
+
+app.use('/', indexRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
